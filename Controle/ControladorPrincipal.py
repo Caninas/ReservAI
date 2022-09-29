dd79a1570580707adb6ec36fc9171ca0395b6515
Pedro Caninas <pgcaninas@gmail.com>
2022-09-28 21:11:29 -0300

    base controlador principal

--- Controle/ControladorPrincipal.py
+++ Controle/ControladorPrincipal.py
@@ -1,12 +1,43 @@
 
 #TODO FAZER IMPORTS
+        
 from Limite.TelaPrincipal import TelaPrincipal
 
+from Controle.ControladorGerente import ControladorGerente
+from Controle.ControladorFuncionario import ControladorFuncionario
+from Controle.ControladorHospede import ControladorHospede
+
+
 class ControladorPrincipal:
+    def __init__(self):   
 
-    def __init__(self):
+        self.__controlador_gerente = ControladorGerente(self)
+        self.__controlador_funcionario = ControladorFuncionario(self)
+        self.__controlador_hospede = ControladorHospede(self)
+        
         self.__tela_principal = TelaPrincipal()
-        #TODO IMPORTAR TELAS E CONTROLADORES
 
-    def inicializa_sistema(self):
+    def iniciar_sistema(self):
         self.__tela_principal.mostra_mensagem("BEM VINDO AO RESERVAI")
+
+
+    def encerrar_sistema(self):
+        exit(0)
+
+    def tela_gerente(self):
+        self.__controlador_gerente.abre_tela()
+
+    def tela_funcionario(self):
+        self.__controlador_funcionario.abre_tela()
+
+    def tela_hospede(self):
+        self.__controlador_hospede.abre_tela()
+
+    # def abre_tela(self):
+        # lista_opçoes = {1: self.tela_funcionario, 2: self.tela_hospede, 0: self.encerrar_sistema}
+
+        # while True:
+        #     opçao = self.__tela_principal.opcoes()
+        #     funçao = lista_opçoes[opçao]
+        #     self.__tela_principal.close()
+        #     funçao()
