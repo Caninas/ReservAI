1d6164057ac17bd8546636b94288cbf09811bf81
Jose Carlos <zezinho158@gmail.com>
2022-10-01 20:47:40 -0300

    controlador func

---
+++ Controle/ControladorFuncionario.py
@@ -0,0 +1,48 @@
+from Persistencia.DAOfuncionario import DAOfuncionario
+from Limite.TelaFuncionario import TelaFuncionario
+from Entidade.Funcionario import Funcionario
+
+import PySimpleGUI as sg
+
+
+class ControladorFuncionario:
+    def __init__(self, controlador_sistema, dao_funcionario, cript):
+        self.__funcionario_dao = dao_funcionario
+        self.__controlador_sistema = controlador_sistema
+        self.__fernet = cript
+        
+        self.__tela_funcionario = TelaFuncionario()
+
+    @property
+    def funcionarios(self):
+        return self.__funcionario_dao.get_all()
+
+    def verificar_se_nome_existe(self, nome):
+        for funcionario in self.__funcionario_dao.get_all():
+            if funcionario.nome == nome:
+                return True
+        return False
+        
+    def cadastrar(self, valores):
+        senha = self.__fernet.encrypt(valores['senha'].encode())
+        funcionario = Funcionario(valores["nome"], valores["usuario"], senha, valores["cpf"], valores["data_nascimento"], valores["telefone"], valores["email"])
+        self.__funcionario_dao.add(funcionario)
+        print("adicionado")
+
+
+    def abre_tela(self):
+        lista_opçoes = {}
+
+        while True:
+            opçao, valores = self.__tela_funcionario.opçoes_menu()
+
+            if opçao == None or opçao == 0 or opçao == sg.WIN_CLOSED:
+                self.__tela_funcionario.close_menu()
+                self.__controlador_sistema.encerrar_sistema()
+                break
+            
+            print(opçao, valores)
+            lista_opçoes[opçao]()
+                
+            self.__tela_funcionario.close_menu()
+
