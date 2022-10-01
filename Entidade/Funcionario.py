c87d19cf0d0c786da5c512cc9541bad0a3a4ed44
Jose Carlos <zezinho158@gmail.com>
2022-10-01 20:48:52 -0300

    func p1

--- Entidade/Funcionario.py
+++ Entidade/Funcionario.py
@@ -1,15 +1,27 @@
 
 class Funcionario:
-    def __init__(self, nome: str, cpf: str, senha: str):
-        self.__cpf = cpf
-        self.__nome = nome
-        self.__senha = senha
+    def __init__(self, nome: str, usuario: str, senha: str, cpf: str, data_nascimento: str, telefone: str, email: str):
+        self.__nome = nome.strip()
+        self.__usuario = usuario.strip()
+        self.__senha = senha.strip()
+        self.__cpf = int(cpf.strip())
+        self.__data_nascimento = data_nascimento.strip()
+        self.__telefone = telefone.strip()
+        self.__email = email.strip()
 
     @property
     def cpf(self):
         return self.__cpf
 
-    @cpf.setter
-    def cpf(self, cpf: str):
-        self.__cpf = cpf
+    @property
+    def usuario(self):
+        return self.__usuario
+    
+    @property
+    def senha(self):
+        return self.__senha
+    
+    @property
+    def nome(self):
+        return self.__nome
 
