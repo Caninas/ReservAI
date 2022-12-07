9cbc183b5867c5f439a57f614e7150df69f552b1
phleao <pedroleao_@hotmail.com>
2022-12-06 21:23:01 -0300

    relatorio hospede

--- Controle/ControladorRelatorio.py
+++ Controle/ControladorRelatorio.py
@@ -6,6 +6,8 @@ from datetime import datetime as dt
 import PySimpleGUI as sg
 import pandas as pd
 
+from collections import Counter
+
 class ControladorRelatorio:
     def __init__(self, controlador_sistema, controlador_reserva, controlador_hospede, controlador_barco):
         self.__controlador_sistema = controlador_sistema
@@ -125,9 +127,140 @@ class ControladorRelatorio:
         else:
             return 1
 
+    def relatorioHospedes(self):
+        update = False
+        data_i = None
+        data_f = None
+        dados = None
+        while True:
+            opçao, valores = self.__tela_relatorio.opçoes_menu_rel_hospedes(update)
+            
+            print(opçao, valores)
+
+            if opçao == None or opçao == 0 or opçao == sg.WIN_CLOSED:
+                self.__tela_relatorio.close_menu_rel_hospedes()
+                break
+            if opçao == 'exportar':
+                data = {'total_reservas': len(dados[0]),'tempo_medio_estadia(dias)': dados[1],
+                    'ocupacao_media_diaria_quartos(%)': dados[2], 'receita_total(R$)': dados[3], 'receita_media_diaria(R$)': dados[4]}
+                df = pd.DataFrame(data, index=['valores'])
+                data_inicial = data_i.strftime("%m-%d-%Y")
+                data_final = data_f.strftime("%m-%d-%Y")
+                try:
+                    df.to_csv(f"{Path.home()}\Documents\\rel_reserva_{data_inicial}_{data_final}.csv")
+                    self.__tela_relatorio.msg("Relatorio salvo com sucesso em pasta Documents!")
+                except:
+                    self.__tela_relatorio.msg("Falha em salvar relatorio :(")
+                continue
+
+
+            data_i = dt.strptime(valores['data_inicial'], '%d-%m-%y')
+            data_f = dt.strptime(valores['data_final'], '%d-%m-%y')
+
+            update = True       # para a instancia da windows nao ser recriada
+
+            if data_i > data_f:
+                self.__tela_relatorio.msg("Data final não pode ser antes da data inicial!")
+                continue
+
+
+            dados = self.gerarDadosHospedes()
+                
+            self.__tela_relatorio.window_menu_rel_hospedes['total_hospedes'].update(f"Total de hóspedes: {dados[0]}")
+            self.__tela_relatorio.window_menu_rel_hospedes['idade_media'].update(f"Média de idade dos hóspedes: {dados[1]:.2f} anos")
+            self.__tela_relatorio.window_menu_rel_hospedes['sexo_masc'].update(f"Hóspedes masculinos: {dados[2]}%")
+            self.__tela_relatorio.window_menu_rel_hospedes['sexo_fem'].update(f"Hóspedes femininos: {dados[3]}%")
+            self.__tela_relatorio.window_menu_rel_hospedes['sexo_outros'].update(f"Hóspedes outros gêneros: {dados[4]}%")
+            self.__tela_relatorio.window_menu_rel_hospedes['cidade1'].update(f"1° cidade mais frequente: {dados[5]}")
+            self.__tela_relatorio.window_menu_rel_hospedes['cidade2'].update(f"2° cidade mais frequente {dados[6]}")
+            self.__tela_relatorio.window_menu_rel_hospedes['cidade3'].update(f"3° cidade mais frequente {dados[7]}")
+            self.__tela_relatorio.window_menu_rel_hospedes['exportar'].update(disabled=False)
+
+
+    def gerarDadosHospedes(self):
+        qtd_hospedes = self.conta_hospedes()
+        media_idade = self.calculaIdadeMedia()
+        fem, masc, outros = self.calcula_genero()
+        cidade1, cidade2, cidade3 = self.calcula_cidades()
+
+        return [qtd_hospedes, media_idade, fem, masc, outros, cidade1, cidade2, cidade3]
+
+    def conta_hospedes(self):
+        qtd = 0
+        for hospede in self.__controlador_hospede.hospedes:
+            qtd = qtd + 1
+        
+        return qtd
+
+    def calculaIdadeMedia(self):
+
+        qtd_hospedes = self.conta_hospedes()
+
+        idade_total = 0
+
+        for hospede in self.__controlador_hospede.hospedes:
+            try:
+                dias = dt.now() - dt.strptime(hospede.data_nascimento , '%d-%m-%Y')
+            except:
+                dias = dt.now() - dt.strptime('09-03-2022' , '%m-%d-%Y')
+            idade = dias.days/365
+            idade_total = idade_total + idade
+       
+        media = idade_total/qtd_hospedes
+
+        return media
+
+
+    def calcula_genero(self):
+        
+        fem = 0
+        masc = 0
+        outros = 0
+
+        qtd_hospedes = self.conta_hospedes()
+
+        for hospede in self.__controlador_hospede.hospedes:
+            if hospede.sexo == 'f' or hospede.sexo == 'F':
+                fem += 1
+            elif hospede.sexo == 'm' or hospede.sexo == 'M':
+                masc += 1
+            else:
+                outros += 1
+
+        media_fem = fem / qtd_hospedes *100
+        media_masc = masc / qtd_hospedes *100
+        media_outros = outros / qtd_hospedes *100
+        
+        print (media_fem, media_masc, media_outros)
+
+        return [media_fem, media_masc, media_outros]
+
+
+    def calcula_cidades(self):
+        lista_cidades = []
+        qtd_cidades = 0
+
+        for hospede in self.__controlador_hospede.hospedes:
+            lista_cidades.append(hospede.end_cidade)
+            qtd_cidades += 1
+
+        frequencia = Counter(lista_cidades).most_common()
+        cidade_1, cidade_2, cidade_3 = frequencia[0], frequencia[1], frequencia[2]
+
+        porc_1 = cidade_1[1] / qtd_cidades * 100
+        porc_2 = cidade_2[1] / qtd_cidades * 100
+        porc_3 = cidade_3[1] / qtd_cidades * 100
+
+        resultado1 = (f"{cidade_1[0]}: {porc_1}%")
+        resultado2 = (f"{cidade_2[0]}: {porc_2}%")
+        resultado3 = (f"{cidade_3[0]}: {porc_3}%")
+
+        print (resultado1, resultado2, resultado3)
+        return [resultado1, resultado2, resultado3]
+
 
     def abre_tela(self):                 # clica quarto mapa (recebe numero dele aqui (botao) e dia selecionado)
-        lista_opçoes = {"rel_reservas": self.relatorioReservas, "rel_hospedes": print,
+        lista_opçoes = {"rel_reservas": self.relatorioReservas, "rel_hospedes": self.relatorioHospedes,
                         "rel_passeios": print}
         
         while True:
--- Limite/TelaRelatorio.py
+++ Limite/TelaRelatorio.py
@@ -8,7 +8,11 @@ class TelaRelatorio:
     @property
     def window_menu_rel_reservas(self):
         return self.__window_menu_rel_reservas
-        
+
+    @property
+    def window_menu_rel_hospedes(self):
+        return self.__window_menu_rel_hospedes
+
     def menu(self):
         layout = [
             [sg.Text("Relatórios", font=("Arial", 20))],
@@ -73,6 +77,68 @@ class TelaRelatorio:
 
             return button, values
 
+
+
+
+
+    def menu_rel_hospedes(self):
+        layout = [
+            [sg.Text("Selecione o período desejado: ", font=("Arial", 15))],
+            [sg.Text('Data Inicial: '), sg.Input(key='data_inicial',size=(20,1)), sg.CalendarButton('Abrir Calendário', title='Selecione a data inicial', no_titlebar=False, format='%d-%m-%y', close_when_date_chosen=False, target='data_inicial', month_names=('Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'), day_abbreviations=('S', 'T', 'Q', 'Q', 'S', 'S', 'D'))],
+            [sg.Text('Data Final: '), sg.Input(key='data_final', size=(20,1)), sg.CalendarButton('Abrir Calendário', title='Selecione a data do final', no_titlebar=False, format='%d-%m-%y', close_when_date_chosen=False, target='data_final', month_names=('Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'), day_abbreviations=('S', 'T', 'Q', 'Q', 'S', 'S', 'D'))],
+            [sg.Button("Visualizar", key=1, size=(20,1), pad=((0,0),(10,0)))],
+            [sg.Button("Voltar", size=(20, 1), key=0)],
+            [sg.Button("Exportar", size=(20, 1), key='exportar', disabled=True)],
+            
+            [sg.Text(key="total_hospedes", font=("Arial", 12), pad=((0,0),(25,0)))],
+            [sg.Text(key="idade_media", font=("Arial", 12))],
+            [sg.Text(key="sexo_masc", font=("Arial", 12))],
+            [sg.Text(key="sexo_fem", font=("Arial", 12))],
+            [sg.Text(key="sexo_outros", font=("Arial", 12))],
+            [sg.Text(key="cidade1", font=("Arial", 12))],
+            [sg.Text(key="cidade2", font=("Arial", 12))],
+            [sg.Text(key="cidade3", font=("Arial", 12))],
+        ]
+        
+
+        self.__window_menu_rel_hospedes = sg.Window('MENU', size=(800, 450), element_justification="c").Layout(layout)
+    
+    def opçoes_menu_rel_hospedes(self, update=False):
+        if not update:
+            self.menu_rel_hospedes()
+        while True:    
+            button, values = self.__window_menu_rel_hospedes.Read()
+
+            if button == None or button == 0 or button == sg.WIN_CLOSED:
+                return button, values
+
+            values.pop("Abrir Calendário") 
+            values.pop("Abrir Calendário0")
+
+            vazio = False
+
+            for valor in values.values():
+                if valor == "" or valor == None:
+                    print(valor)
+                    vazio = True
+                    break
+
+            if vazio == True:
+                self.msg("Todos os campos devem ser preenchidos!")
+                continue
+
+            data1 = values["data_inicial"].split("-")
+            data2 = values["data_final"].split("-")
+
+            if any([len(x) != 2 or len(data1) != 3 for x in data1]) or any([len(x) != 2 or len(data2) != 3 for x in data2]):
+                self.msg("O formato da data deve ser Ex: '29-12-22'")
+                continue
+
+
+            return button, values
+
+
+
     def opçoes_menu(self):
         self.menu()
         button, values = self.__window_menu.Read()
@@ -87,6 +153,9 @@ class TelaRelatorio:
 
     def close_menu_rel_reservas(self):
         self.__window_menu_rel_reservas.Close()
+
+    def close_menu_rel_hospedes(self):
+        self.__window_menu_rel_hospedes.Close()
 
     def msg(self, msg):
         sg.Popup(msg)
